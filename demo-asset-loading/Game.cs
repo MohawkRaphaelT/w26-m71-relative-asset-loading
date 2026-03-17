6b4b21d1fc8eb568ffe4b6580150255bd34652ce
RaphaelT <raphael.tetreault@mohawkcollege.ca>
2026-03-17 16:38:05 -0400

    Add asset loading (absolute)

--- demo-asset-loading/Game.cs
+++ demo-asset-loading/Game.cs
@@ -11,14 +11,17 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-
+        Texture2D characters = Graphics.LoadTexture("C:\\Users\\000906269\\source\\repos\\w26-m71-relative-asset-loading\\assets\\graphics\\characters.gif");
+        Sound sound = Audio.LoadSound("C:\\Users\\000906269\\source\\repos\\w26-m71-relative-asset-loading\\assets\\audio\\sound.wav");
+        Sound target = Audio.LoadSound("C:\\Users\\000906269\\source\\repos\\w26-m71-relative-asset-loading\\assets\\audio\\target.ogg");
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-
+            Window.SetTitle("Asset Load Demo");
+            Window.SetSize(400, 400);
         }
 
         /// <summary>
@@ -26,7 +29,20 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.OffWhite);
+            //
+            Graphics.Draw(characters, Window.Size / 2);
 
+            // Play 'sound'
+            if (Input.IsKeyboardKeyPressed(KeyboardInput.Q))
+            {
+                Audio.Play(sound);
+            }
+            // Play `target`
+            if (Input.IsKeyboardKeyPressed(KeyboardInput.W))
+            {
+                Audio.Play(target);
+            }
         }
     }
 
